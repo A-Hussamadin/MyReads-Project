1f9bc636924d740218c011382214ff2fb341c346
A-Hussamadin <h.abushelha@gmail.com>
2017-09-08 05:41:11 +0300

    Adding ReactRouterDom, SearchPage component and update the code accordingly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 import BooksList from './BooksList';
+import SearchPage from './SearchPage';
 class BooksApp extends React.Component {
   state = {
     books :  [],
@@ -40,35 +42,17 @@ updateShelf = (book, shelf) =>{
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+          <Route path='/search' component={SearchPage}/>
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
+          
+          <Route  exact path='/' render={() =>(
             <BooksList 
             books={this.state.books}
             updateShelf={this.updateShelf}
             onSearchClick={this.showSearchPage}
             />
-          </div>
+        )} />
+         
         )}
       </div>
     )
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Shelf from './Shelf';
-
+import { Link } from 'react-router-dom';
 class BooksList extends Component{
     render(){
       
@@ -10,7 +10,7 @@ class BooksList extends Component{
 
    
         return (
-         
+          
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -49,7 +49,7 @@ class BooksList extends Component{
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.props.onSearchClick()}>Add a book</a>
+              <Link to='/search'>Add a book</Link>
             </div>
           </div>
         )
